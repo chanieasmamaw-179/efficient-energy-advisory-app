938496f9999c54b6107aecd0ef439f51ba32e4be
Asmamaw Yehun (Ph.D.) <chanieasmamaw@yahoo.com>
2024-12-02 20:40:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -47,5 +47,5 @@ async def root():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="127.0.0.1", port=port)
 
